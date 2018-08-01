952f318e562548fb7eb009970fc13daa09cdb6ad
Bailey Belvis <bailey.belvis@match.com>
2018-08-01 10:08:00 -0500

    [03] initial extension.js language server impl

--- src/extension.js
+++ src/extension.js
@@ -2,6 +2,13 @@ const vscode = require('vscode');
 const fs = require('fs');
 const open = require('open');
 const watch = require('node-watch');
+const path = require('path');
+import {
+	LanguageClient,
+	LanguageClientOptions,
+	ServerOptions,
+	TransportKind
+} from 'vscode-languageclient';
 
 const oadServer = require('./oadServer');
 const oadLogger = require('./oadLogger');
@@ -15,165 +22,200 @@ var logger = oadLogger();
 
 var servers = {};
 
+var languageServerDebugOptions = { execArgv: ['--nolazy', '--inspect=6009'] };
+var languageClient;
+
 function start(openApiFile, targetDir, port, hostname, openBrowser, context) {    
-    var server = oadServer(port, openApiFile);
-    servers[openApiFile] = server;
+	var server = oadServer(port, openApiFile);
+	servers[openApiFile] = server;
 
-    logger.log("Created server for: " + openApiFile + " on port: " + port);
+	logger.log("Created server for: " + openApiFile + " on port: " + port);
 
-    server.io.on('connection', function(socket) {
-        var socketKey = ++server.lastSocketKey;
-        server.connections[socketKey] = socket;
-        socket.on('disconnect', function() {
-            delete server.connections[socketKey];
-        });
+	server.io.on('connection', function(socket) {
+		var socketKey = ++server.lastSocketKey;
+		server.connections[socketKey] = socket;
+		socket.on('disconnect', function() {
+			delete server.connections[socketKey];
+		});
 
-        logger.log("Connection for: " + openApiFile);
+		logger.log("Connection for: " + openApiFile);
 
-        socket.on('uiReady', function(data) {
-            util.bundle(openApiFile).then(function (bundled) {
-                logger.log("Sending init file to: " + openApiFile);
-                var bundleString = JSON.stringify(bundled, null, 2);
-                server.io.emit('updateSpec', bundleString);
-            }, function (err) {
-                server.io.emit('showError', err);
-                logger.log('Error: ' + err);
-            });
-        });
-    });
-    
-    watch(targetDir, {recursive: true}, function(eventType, name) {
-        util.bundle(openApiFile).then(function (bundled) {
-            logger.log("File changed. Sent updated spec to the browser. File: " + openApiFile + " port: " + server.port);
-            var bundleString = JSON.stringify(bundled, null, 2);
-            server.io.emit('updateSpec', bundleString);
-        }, function (err) {
-            logger.log('Error: ' + err);
-            server.io.emit('showError', err);
-        });
-    });
+		socket.on('uiReady', function(data) {
+			util.bundle(openApiFile).then(function (bundled) {
+				logger.log("Sending init file to: " + openApiFile);
+				var bundleString = JSON.stringify(bundled, null, 2);
+				server.io.emit('updateSpec', bundleString);
+			}, function (err) {
+				server.io.emit('showError', err);
+				logger.log('Error: ' + err);
+			});
+		});
+	});
+	
+	watch(targetDir, {recursive: true}, function(eventType, name) {
+		util.bundle(openApiFile).then(function (bundled) {
+			logger.log("File changed. Sent updated spec to the browser. File: " + openApiFile + " port: " + server.port);
+			var bundleString = JSON.stringify(bundled, null, 2);
+			server.io.emit('updateSpec', bundleString);
+		}, function (err) {
+			logger.log('Error: ' + err);
+			server.io.emit('showError', err);
+		});
+	});
 
-    server.listen(hostname);
-    if (openBrowser){
-        open(server.serverUrl);
-    } else {
-        createViewer(context, port, openApiFile);
-    }
+	server.listen(hostname);
+	if (openBrowser){
+		open(server.serverUrl);
+	} else {
+		createViewer(context, port, openApiFile);
+	}
   }
 
 function build (openApiFile, bundleTo) {
-    util.bundle(openApiFile).then(function (bundled) {
-        var bundleString = JSON.stringify(bundled, null, 2);
-        if (typeof bundleTo === 'string') {
-          fs.writeFile(bundleTo, bundleString, function(err) {
-            if (err) {
-                logger.log(err);
-              return;
-            }
-            logger.log('Saved bundle file at ' + bundleTo);
-          });
-        }
-      }, function (err) {
-        logger.log(err);
-      });
+	util.bundle(openApiFile).then(function (bundled) {
+		var bundleString = JSON.stringify(bundled, null, 2);
+		if (typeof bundleTo === 'string') {
+		  fs.writeFile(bundleTo, bundleString, function(err) {
+			if (err) {
+				logger.log(err);
+			  return;
+			}
+			logger.log('Saved bundle file at ' + bundleTo);
+		  });
+		}
+	  }, function (err) {
+		logger.log(err);
+	  });
   }
 
 function createViewer(context, port, fileName){
-    logger.log("Creating Viewer for: " + fileName);
-    var viewer = oadViewer(context, port, previewUri);
-    var ds = viewer.register();
-    context.subscriptions.push(...ds);
-    viewer.setPort(servers[fileName].port);
-    viewer.display();
-    viewer.update();
+	logger.log("Creating Viewer for: " + fileName);
+	var viewer = oadViewer(context, port, previewUri);
+	var ds = viewer.register();
+	context.subscriptions.push(...ds);
+	viewer.setPort(servers[fileName].port);
+	viewer.display();
+	viewer.update();
 }
 
 function runDesigner(context, openBrowser) {
-    logger.log('Running OpenApiDesigner');
+	logger.log('Running OpenApiDesigner');
 
-    var config = vscode.workspace.getConfiguration('openApiDesigner');
-    var port = config.defaultPort || defaultPort;
+	var config = vscode.workspace.getConfiguration('openApiDesigner');
+	var port = config.defaultPort || defaultPort;
 
-    if(lastPortUsed != 0) {
-        port = ++lastPortUsed;
-    } else {
-        lastPortUsed = port;
-    }        
+	if(lastPortUsed != 0) {
+		port = ++lastPortUsed;
+	} else {
+		lastPortUsed = port;
+	}        
 
-    var fileName = getActiveFile();
-    var filePath = getActivePath();
-        
-    if(!(servers[fileName] && servers[fileName].listening)) {
-        start(fileName, filePath, port, "localhost", openBrowser, context);
-    } else {
-        // Server exists, update viewer.
-        createViewer(context, port, fileName);
-    }    
+	var fileName = getActiveFile();
+	var filePath = getActivePath();
+		
+	if(!(servers[fileName] && servers[fileName].listening)) {
+		start(fileName, filePath, port, "localhost", openBrowser, context);
+	} else {
+		// Server exists, update viewer.
+		createViewer(context, port, fileName);
+	}    
 }
 
 function getActiveFile() {
-    var editor = vscode.window.activeTextEditor;
-    if (!editor) {
-        return;
-    }
+	var editor = vscode.window.activeTextEditor;
+	if (!editor) {
+		return;
+	}
 
-    var doc = editor.document;
-    return doc.fileName.toLowerCase();
+	var doc = editor.document;
+	return doc.fileName.toLowerCase();
 }
 
 function getActivePath() {
-    var activeFile = getActiveFile();
-    return util.getPathFromFile(activeFile);
+	var activeFile = getActiveFile();
+	return util.getPathFromFile(activeFile);
 }
 
 function shutdown() {
 
-    Object.keys(servers).forEach(serverKey =>function(serverKey){
-        servers[serverKey].close();
-        delete servers[serverKey];
-    });
+	Object.keys(servers).forEach(serverKey =>function(serverKey){
+		servers[serverKey].close();
+		delete servers[serverKey];
+	});
 }
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 function activate(context) {
+	// LANGUAGE SERVER
+	var serverModule = context.asAbsolutePath(path.join('server', 'out', 'server.js'));
 
-    context.subscriptions.push(vscode.commands.registerCommand('openapidesigner.runDesigner', function () {
-        var config = vscode.workspace.getConfiguration('openApiDesigner');
-        var openBrowser = config.previewInBrowser || false;
-        runDesigner(context, openBrowser);
-    }));
+	var serverOptions = {
+		run: { module: serverModule, transport: TransportKind.ipc},
+		debug: { module: serverModule, transport: TransportKind.ipc, options: languageServerDebugOptions}
+	};
 
-    context.subscriptions.push(vscode.commands.registerCommand('openapidesigner.runDesignerInSideView', function () {
-        runDesigner(context, false);
-    }));
+	var clientOptions = {
+		// Register the server for plain text documents
+		documentSelector: [{ scheme: 'file', language: 'plaintext' }],
+		synchronize: {
+			// Notify the server about file changes to '.clientrc files contained in the workspace
+			fileEvents: context.workspace.createFileSystemWatcher('**/.clientrc')
+		}
+	};
+	
+	languageClient = new LanguageClient(
+		'languageServerExample',
+		'Language Server Example',
+		serverOptions,
+		clientOptions
+	);
+	
+	languageClient.start();
+	//////////////////
 
-    context.subscriptions.push(vscode.commands.registerCommand('openapidesigner.runDesignerInBrowser', function () {
-        runDesigner(context, true);
-    }));
+	context.subscriptions.push(vscode.commands.registerCommand('openapidesigner.runDesigner', function () {
+		var config = vscode.workspace.getConfiguration('openApiDesigner');
+		var openBrowser = config.previewInBrowser || false;
+		runDesigner(context, openBrowser);
+	}));
 
-    context.subscriptions.push(vscode.commands.registerCommand('openapidesigner.compileFiles', function () {
-        build(getActiveFile(), getActivePath() + "/openapi.json");
-    }));
+	context.subscriptions.push(vscode.commands.registerCommand('openapidesigner.runDesignerInSideView', function () {
+		runDesigner(context, false);
+	}));
 
-    vscode.workspace.onDidCloseTextDocument((textDocument) => {
-        if(textDocument.uri.toString() == previewUri){
-            logger.log('Killing server');
-            shutdown();
-        }
-    });
+	context.subscriptions.push(vscode.commands.registerCommand('openapidesigner.runDesignerInBrowser', function () {
+		runDesigner(context, true);
+	}));
 
-    vscode.workspace.onDidChangeConfiguration((event) => {
-        if (!event.affectsConfiguration('openapidesigner')) return;
+	context.subscriptions.push(vscode.commands.registerCommand('openapidesigner.compileFiles', function () {
+		build(getActiveFile(), getActivePath() + "/openapi.json");
+	}));
 
-        lastPortUsed = 0;
-    });
+	vscode.workspace.onDidCloseTextDocument((textDocument) => {
+		if(textDocument.uri.toString() == previewUri){
+			logger.log('Killing server');
+			shutdown();
+		}
+	});
+
+	vscode.workspace.onDidChangeConfiguration((event) => {
+		if (!event.affectsConfiguration('openapidesigner')) return;
+
+		lastPortUsed = 0;
+	});
 }
 
 // this method is called when your extension is deactivated
 function deactivate() {
-    console.log('Shutting down.');
-    shutdown();
+	console.log('Shutting down.');
+	shutdown();
+
+	languageClient
+	if (!languageClient) {
+		return undefined;
+	}
+	return languageClient.stop();
 }
 
 exports.activate = activate;
