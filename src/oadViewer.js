750b96aba267c024abfcb39391158f10f4cd3028
Bailey <baileyb622@gmail.com>
2019-04-14 13:36:19 -0500

    what a disaster

--- src/oadViewer.js
+++ src/oadViewer.js
@@ -18,10 +18,50 @@ class Viewer {
         this.currentPanel = undefined;
     }
 
-    provideTextDocumentContent(uri) {
-        var port = this.port;
-        var html = fs.readFileSync(path.join(__dirname, "..", "index.html")).toString('utf-8');        
-        return html;
+    provideTextDocumentContent() {
+        try{
+            var indexPath = path.join(__dirname, "..", "index.html");
+            logger.log('Index.html: ' + indexPath);
+
+            var baseHtml = fs.readFileSync(indexPath).toString('utf-8');
+
+            // if loading in browser
+            //var finalHtml = baseHtml.replace(/{PREVIEW_URI}/gi, "http://localhost:9005");
+            //return finalHtml;
+            
+            //Else Get resource paths
+            var swagger_editor_dist_path = path.join(this.context.extensionPath, 'node_modules', 'swagger-editor-dist');
+
+            var swagger_editor_css = vscode.Uri.file(path.join(swagger_editor_dist_path, 'swagger-editor.css')).with({ scheme: 'vscode-resource' }).toString(true);
+            var swagger_editor_favicon_32_png = vscode.Uri.file(path.join(swagger_editor_dist_path, "favicon-32x32.png")).with({ scheme: 'vscode-resource' }).toString(true);
+            var swagger_editor_favicon_16_png = vscode.Uri.file(path.join(swagger_editor_dist_path, "favicon-16x16.png")).with({ scheme: 'vscode-resource' }).toString(true);
+            var swagger_editor_bundle_js = vscode.Uri.file(path.join(swagger_editor_dist_path, "swagger-editor-bundle.js")).with({ scheme: 'vscode-resource' }).toString(true);
+            var swagger_editor_standalone_preset_js = vscode.Uri.file(path.join(swagger_editor_dist_path, "swagger-editor-standalone-preset.js")).with({ scheme: 'vscode-resource' }).toString(true);
+            
+            var socket_io = vscode.Uri.file(path.join(this.context.extensionPath, "node_modules", "socket.io", "socket.io.js")).with({ scheme: 'vscode-resource' }).toString(true);
+            
+            // Replace resource paths
+            //var finalHtml = baseHtml.replace(/{SERVER_URL}/gi, "http://localhost:9005");
+            // finalHtml = finalHtml.replace("/node_modules/swagger-editor-dist/favicon-32x32.png", swagger_editor_css);
+            // finalHtml = finalHtml.replace("/node_modules/swagger-editor-dist/favicon-16x16.png", swagger_editor_favicon_32_png);
+            // finalHtml = finalHtml.replace("/node_modules/swagger-editor-dist/favicon-16x16.png", swagger_editor_favicon_16_png);
+            // finalHtml = finalHtml.replace("/node_modules/swagger-editor-dist/swagger-editor-bundle.js", swagger_editor_bundle_js);
+            // finalHtml = finalHtml.replace("/node_modules/swagger-editor-dist/swagger-editor-standalone-preset.js", swagger_editor_standalone_preset_js);
+            // finalHtml = finalHtml.replace("/socket.io/socket.io.js", socket_io);
+
+            var finalHtml = `
+			<html>
+				<body style="margin:0px;padding:0px;background:#fafafa;">
+					<div style="position:fixed;height:100%;width:100%;">
+					<iframe src="http://localhost:${this.port}" frameborder="0" style="overflow:hidden;height:100%;width:100%" height="100%" width="100%"></iframe>
+					</div>
+                </body>
+            <html>`;
+		
+            return finalHtml;
+        } catch (e){
+            logger.log(e);
+        }
     }
 
     display() {
@@ -39,11 +79,12 @@ class Viewer {
             {
                 enableScripts: true,
                 retainContextWhenHidden: true,
-                localResourceRoots: [vscode.Uri.file(this.context.extensionPath)]
+                //localResourceRoots: [vscode.Uri.file(path.join(this.context.extensionPath, 'node_modules'))]
             } 
         );
-
-        this.currentPanel.onDidDispose(() => this.dispose(), null, null);
+        
+        this.currentPanel.webview.html = this.provideTextDocumentContent();
+        //this.currentPanel.onDidDispose(() => this.dispose(), null, null);
     }
 
     register() {
